7f14a40adc0808c8cf9f1030b2eff92ff7326077
morooi <zsjynt@qq.com>
2024-08-12 21:06:47 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 go build -o update_cert update_tencent_cloud_cdn_cert.go
-go build -o refresh_cdn update_tencent_cloud_cdn_cert.go
+go build -o refresh_cdn refresh_tencent_cloud_cdn.go
 go build -o audit_time_analyze audit_time_analyze.go
